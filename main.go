b747eb02f8cb864b639e8b35ab0d556d2fcf261f
Jorge Reyero <jorgereyero@gmail.com>
2020-04-07 19:24:13 +0200

    use http.HandleFunc

--- main.go
+++ main.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
-type myHandler struct {
-	greetings string
-}
-
-func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("%v world.", mh.greetings)))
-}
-
 func main() {
-	http.Handle("/", myHandler{greetings: "Hello"})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!!"))
+	})
 	http.ListenAndServe(":8080", nil)
 }
