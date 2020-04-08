0563efca7b0fb9b1b693ddf936352eccd3e524be
Jorge Reyero <jorgereyero@gmail.com>
2020-04-08 13:06:26 +0200

    convert static html to templates

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"text/template"
 )
 
 func main() {
+	templates := populateTemplates()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!!"))
+		requestedPath := r.URL.Path[1:]
+		t := templates.Lookup(requestedPath + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
 	})
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+
 	http.ListenAndServe(":8080", nil)
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*"))
+	return result
 }
