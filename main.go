d28eff5aee5dbd31f7f1b01fa2ebe8baccdef2b2
Jorge Reyero <jorgereyero@gmail.com>
2020-04-09 12:39:31 +0200

    use template composition

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"text/template"
 )
 
@@ -10,7 +12,7 @@ func main() {
 	templates := populateTemplates()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestedPath := r.URL.Path[1:]
-		t := templates.Lookup(requestedPath + ".html")
+		t := templates[requestedPath+".html"]
 		if t != nil {
 			err := t.Execute(w, nil)
 			if err != nil {
@@ -26,9 +28,35 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func populateTemplates() *template.Template {
-	result := template.New("templates")
+func populateTemplates() map[string]*template.Template {
+	result := make(map[string]*template.Template)
 	const basePath = "templates"
-	template.Must(result.ParseGlob(basePath + "/*"))
+	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
+	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+	dir, err := os.Open(basePath + "/content")
+	if err != nil {
+		panic("Failed to open template blocks directory: " + err.Error())
+	}
+	fis, err := dir.Readdir(-1)
+	if err != nil {
+		panic("Failed to read contents of content directory " + err.Error())
+	}
+	for _, fi := range fis {
+		f, err := os.Open(basePath + "/content/" + fi.Name())
+		if err != nil {
+			panic("Failed to open template " + err.Error())
+		}
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic("Failed to read content from file " + err.Error())
+		}
+		f.Close()
+		tmpl := template.Must(layout.Clone())
+		_, err = tmpl.Parse(string(content))
+		if err != nil {
+			panic("Failed to parse contents of '" + fi.Name() + "' as template")
+		}
+		result[fi.Name()] = tmpl
+	}
 	return result
 }
