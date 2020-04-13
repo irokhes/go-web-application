769fdec32042f451e4f58bb4e3adc274a5557a5e
Jorge Reyero <jorgereyero@gmail.com>
2020-04-13 17:20:53 +0200

    use looping

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 		case "shop":
 			context = viewmodel.NewShop()
 		default:
-			context = viewmodel.NewBase()
+			context = viewmodel.NewHome()
 		}
 		if template != nil {
 			err := template.Execute(w, context)
