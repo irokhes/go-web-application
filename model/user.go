a5e43a5848dc0d5a2f0715e0c54379aab541fd49
Jorge Reyero <jorgereyero@gmail.com>
2020-05-02 10:59:09 +0200

    update database command

--- model/user.go
+++ model/user.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"encoding/base64"
 	"fmt"
+	"log"
 	"time"
 )
 
@@ -27,16 +28,24 @@ func Login(email, password string) (*User, error) {
 	hasher.Write([]byte(password))
 	pwd := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
 	row := db.QueryRow(`
-		SELECT id, email, firstname, lastname
+		SELECT id, email, firstname, lastname, lastlogin
 		FROM public.user
 		WHERE email = $1
 			AND password = $2`, email, pwd)
-	err := row.Scan(&result.ID, &result.Email, &result.FirstName, &result.LastName)
+	err := row.Scan(&result.ID, &result.Email, &result.FirstName, &result.LastName, &result.LastLogin)
 	switch {
 	case err == sql.ErrNoRows:
 		return nil, fmt.Errorf("User not found")
 	case err != nil:
 		return nil, err
+	}
+	t := time.Now()
+	_, err = db.Exec(`
+	UPDATE public.user
+	SET lastlogin = $1
+	WHERE id = $2`, t, result.ID)
+	if err != nil {
+		log.Printf("Failed to update login time for user %v to %v: %v", t, result.Email, err)
 	}
 	return result, nil
 
